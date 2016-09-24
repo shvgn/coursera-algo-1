7067884a042c175ca817344aba7e599f646463c2
Evgeny Shevchenko <shvgn@protonmail.com>
2016-09-24 15:54:50 +0300

    Golang merge sort implementation, not all tests pass

---
+++ week1/merge_sort.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+)
+
+// merge merges two slices and returns the resulting sorted slice
+// and the number of inversions in it
+func merge(s1, s2 []int) ([]int, int) {
+	var i, j, x1, x2, inversions int
+	var s1finished, s2finished bool
+
+	l1 := len(s1)
+	l2 := len(s2)
+	length := l1 + l2
+	merged := make([]int, length, length)
+
+	for i < l1 || j < l2 {
+		if i < l1 {
+			x1 = s1[i]
+		} else {
+			s1finished = true
+		}
+
+		if j < l2 {
+			x2 = s2[j]
+		} else {
+			s2finished = true
+		}
+
+		if s1finished || (!s2finished && x1 > x2) {
+			merged[i+j] = x2
+			j += 1
+			if !s1finished {
+				inversions += l1 - i
+			}
+		} else if s2finished || x1 <= x2 {
+			merged[i+j] = x1
+			i += 1
+		}
+	}
+	return merged, inversions
+}
+
+// merge_sort returns the sorted slice and the number of inversions in it
+func mergeSort(s []int) ([]int, int) {
+	if len(s) <= 1 {
+		return s, 0
+	}
+	if len(s) == 2 {
+		x1, x2 := s[0], s[1]
+		inversions := 0
+		if x1 > x2 {
+			s[0], s[1] = x1, x2
+			inversions = 1
+		}
+		return s, inversions
+	}
+	divider := len(s) / 2
+	s1, inv1 := mergeSort(s[:divider])
+	s2, inv2 := mergeSort(s[divider:])
+	merged, inv3 := merge(s1, s2)
+	inversions := inv1 + inv2 + inv3
+	return merged, inversions
+}
+
+func main() {
+	fmt.Println("epta")
+
+}
